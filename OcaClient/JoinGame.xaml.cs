2f41236a3e18bba1d52df25bec3385d83dde6dc7
Mario Alberto Jimenez Jimenez <90919789+MarioAlJim@users.noreply.github.com>
2022-10-25 12:31:07 -0500

    Agregados ventanas WPF

--- OcaClient/JoinGame.xaml.cs
+++ OcaClient/JoinGame.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace OcaClient
 {
     /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
+    /// Lógica de interacción para JoinGame.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class JoinGame : Window
     {
-        public MainWindow()
+        public JoinGame()
         {
             InitializeComponent();
         }
