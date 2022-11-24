1241e5ad8fd80b53d1e4474b569e991007173340
Jonatan Alarcón <jonatan7890@hotmail.com>
2022-11-23 22:40:37 -0600

    Lógica de movimiento de ficha
    Validaciones para colocar en las coordenadas las posiciones de las fichas del jugador

--- OcaClient/GameBoard.xaml.cs
+++ OcaClient/GameBoard.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace OcaClient
 {
@@ -20,18 +21,28 @@ namespace OcaClient
     /// </summary>
     public partial class GameBoard : Window
     {
+        private DispatcherTimer timer;
         public GameBoard()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += timer_tick;
+            timer.Start();
         }
 
-       
+        private void timer_tick(object sender, EventArgs e)
+        {
+            
+        }
+
         private void roll_Dice(object sender, RoutedEventArgs e)
         {
             Random random = new Random();
             BitmapImage bitmap = new BitmapImage();
             
-            int diceNumber = random.Next(1, 7);       ;
+            int diceNumber = random.Next(1,7);
+            move(20);
             switch (diceNumber)
             {
                 case 1:
@@ -77,6 +88,87 @@ namespace OcaClient
                     img_dice.Source= bitmap;                   
                     break;
             }
+            
         }
+
+        public void move(int pos)
+        {
+
+            int posX;
+            int posY;
+            int spaces = 65;
+            if (pos > 0 && pos < 9)
+            {
+                posX = pos * spaces;
+                posY = 415;              
+                setPieces(posX, posY, 0);
+
+            }
+            else
+            {
+                if (pos < 18)
+                {
+                    posX = pos * spaces;
+                    posY = 1;
+                    //setPieces(posX, posY);
+                }
+                else
+                {
+                    if (pos > 18 && pos < 27)
+                    {
+                        posX = pos * spaces;
+                        posY = 281;
+                        setPieces(posX, posY, 0);
+                    }
+                    else 
+                    {
+                        if (pos < 36)
+                        {
+                            posX = pos * spaces;
+                            posY = 3;
+                            //setPieces(posX, posY);
+                        }
+                        else 
+                        { 
+                            if (pos < 45)
+                            {
+                                posX = pos * spaces;
+                                posY = 4;
+                                //setPieces(posX, posY);
+                            }
+                            else
+                            {
+                                if (pos < 54)
+                                {
+                                    posX = pos * spaces;
+                                    posY = 5;
+                                    //setPieces(posX, posY);
+                                }
+                                else
+                                {
+                                    if (pos < 60)
+                                    {
+                                        posX = pos * spaces;
+                                        posY = 6;
+                                        //setPieces(posX, posY);
+                                    }
+                                }
+                            }   
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        public void setPieces(int x, int y, int z)
+        {
+            img_RedChip.Margin = new Thickness(x, y, 0, z);
+               
+        }
+
+
+
     }
 }
